efa1196a44e2019924c8770ac2eb17866d4fdf85
Armando Genis <armando_genis@outlook.com>
2025-01-31 11:55:46 -0600

    ros2 web socket fix error eloquent

--- workspace/ros2_ws/install/camera_testing/share/camera_testing/src/video_publisher.cpp
+++ workspace/ros2_ws/install/camera_testing/share/camera_testing/src/video_publisher.cpp
@@ -11,6 +11,11 @@ private:
     cv::VideoCapture cap_;
     std::string video_path_;
 
+    void publish_frame();
+
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
 public:
     video_publisher(/* args */);
     ~video_publisher();
@@ -25,11 +30,32 @@ video_publisher::video_publisher(/* args */) : Node("camera_reader_node")
         RCLCPP_ERROR(this->get_logger(), "Failed to open video file: %s", video_path_.c_str());
         rclcpp::shutdown();
     }
+
+    image_pub_ = this->create_publisher<sensor_msgs::msg::Image>("/video_frames", 10);
+
+    timer_ = this->create_wall_timer(
+        std::chrono::milliseconds(30),
+        std::bind(&video_publisher::publish_frame, this));
+
     RCLCPP_INFO(this->get_logger(), "\033[1;32m----> camera_reader_node initialized.\033[0m");
 }
 
 video_publisher::~video_publisher()
 {
+}
+
+void video_publisher::publish_frame()
+{
+    cv::Mat frame;
+    if (!cap_.read(frame))
+    {
+        RCLCPP_WARN(this->get_logger(), "Video ended or cannot read frame");
+        rclcpp::shutdown();
+        return;
+    }
+
+    auto msg = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", frame).toImageMsg();
+    image_pub_->publish(*msg);
 }
 
 int main(int argc, char *argv[])
